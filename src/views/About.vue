95debdc50913c9109c4d4f7fcb5d61a87b4a99ea
lodelux <lodelux@live.it>
2021-08-18 23:45:24 +0200

    added about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="mx-3 p-6 bg-gray-900 rounded-xl shadow-xl mt-7 md:mt-20">
+    <h1 class="text-2xl font-bold text-center">How does it work</h1>
+    <p class="mt-6">
+      This website uses an API service from amadeus.com both to find the flight
+      and to gather AI driven predictions on the delay of the flights, more on
+      <a
+        class="underline text-green-300"
+        target="_blank"
+        href="https://developers.amadeus.com/blog/flight-delay-prediction-machine-learning"
+      >
+        here</a
+      >
+    </p>
+    <h1 class="text-2xl font-bold text-center mt-12">Website</h1>
+    <p class="mt-6">
+      This website was developed in around 2 weeks as a small side project
+      mainly to learn some frontend developing, its completely open source and
+      you can find it
+      <a
+        class="underline text-green-300"
+        target="_blank"
+        href="https://github.com/lodelux/flight-prediction"
+      >
+        here</a
+      >
+    </p>
+    <h1 class="text-2xl font-bold text-center mt-12">Feedbacks & Contacts</h1>
+    <p class="mt-6">
+      If you have any feedback or proposal of any kind, you can reach me at
+      <a href="mailto:lodelux2@live.it" class="underline">lodelux2@live.it</a>
+    </p>
   </div>
 </template>
