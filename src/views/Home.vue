0435042159895c80ec41b8fe3d5d63a71831e189
lodelux <lodelux@live.it>
2021-08-20 14:29:32 +0200

    +SEO

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,20 +4,27 @@
     <prediction-block />
   </div>
   <div class="p-2 pb-6 mt-3 md:mt-8 w-full text-center">
-    <h1 class="font-bold text-2xl md:text-3xl">How to use</h1>
+    <h1 class="font-bold text-2xl md:text-3xl">
+      How to get Flight delay prediction
+    </h1>
     <ul class="font-medium text-base mx-2">
       <li class="mt-5 md:mt-10 card">
-        <b class="font-bold block border-b pb-1 md:text-xl">Enter the flight's data</b>
+        <b class="font-bold block border-b pb-1 md:text-xl"
+          >Enter the flight's data</b
+        >
         <p class="block mt-2">
           for example for the flight SU6674 -> SU is the carrier code and 6674
           is the flight number
         </p>
       </li>
       <li class="mt-5 md:mt-10 card">
-        <b class="font-bold block border-b pb-1 md: md:text-xl">Get predictions</b>
+        <b class="font-bold block border-b pb-1 md: md:text-xl"
+          >Get predictions</b
+        >
         <p class="block mt-2">
           we use industry-leading AI tools to compare your flight with the ones
-          in the database, with an
+          in the database and calculate delay predictions for your flight, with
+          an
           <a
             class="underline text-green-300"
             target="_blank"
