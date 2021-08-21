fb132bfeb8544f3158c258f8d5a02af33e499fe7
lodelux <lodelux@live.it>
2021-08-21 12:05:19 +0200

    +seo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,23 +5,23 @@
   </div>
   <div data-nosnippet class="p-2 pb-6 mt-3 md:mt-8 w-full text-center">
     <h1 class="font-bold text-2xl md:text-3xl">
-      Predict if your flight will be delayed or cancelled
+      How to get predictions for the delay of your flight
     </h1>
-    
+
     <ul class="font-medium text-base mx-2">
       <li class="mt-5 md:mt-10 card">
-        <h3 class="font-bold block border-b pb-1 md:text-xl"
-          >Enter the flight's data</h3
-        >
+        <h3 class="font-bold block border-b pb-1 md:text-xl">
+          Enter the flight's data
+        </h3>
         <p class="block mt-2">
           for example for the flight SU6674 -> SU is the carrier code and 6674
           is the flight number
         </p>
       </li>
       <li class="mt-5 md:mt-10 card">
-        <h3 class="font-bold block border-b pb-1 md: md:text-xl"
-          >Get predictions</h3
-        >
+        <h3 class="font-bold block border-b pb-1 md: md:text-xl">
+          Get predictions
+        </h3>
         <p class="block mt-2">
           we use industry-leading AI tools to compare your flight with the ones
           in the database and calculate delay predictions for your flight, with
